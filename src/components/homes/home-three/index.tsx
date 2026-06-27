67d4e40918b728cb2507c4be37217d13b2d35d2b
pribakil <bakengailunga@gmail.com>
2026-06-27 10:18:14 -0400

    update: program section data made dynamic

--- src/components/homes/home-three/index.tsx
+++ src/components/homes/home-three/index.tsx
@@ -1,39 +1,39 @@
-import HeaderThree from "@/layouts/headers/HeaderThree"
-import Banner from "./Banner"
-import Countdown from "./Countdown"
-import Feature from "./Feature"
-import About from "./About"
-import Team from "./Team"
-import Pricing from "./Pricing"
-import FooterOne from "@/layouts/footers/FooterOne"
-import Event from "./Event"
-import TextSlider from "../home-one/TextSlider"
-import Experience from "../home-one/Experience"
-import Testimonial from "./Testimonial"
-import Brand from "../home-two/Brand"
-import Blog from "./Blog"
+import HeaderThree from "@/layouts/headers/HeaderThree";
+import Banner from "./Banner";
+import Countdown from "./Countdown";
+import Feature from "./Feature";
+import About from "./About";
+import Team from "./Team";
+import Pricing from "./Pricing";
+import FooterOne from "@/layouts/footers/FooterOne";
+import TextSlider from "../home-one/TextSlider";
+import Experience from "../home-one/Experience";
+import Testimonial from "./Testimonial";
+import Brand from "../home-two/Brand";
+import Blog from "./Blog";
+import Schedule from "../home-one/Schedule";
 
 const HomeThree = () => {
-   return (
-      <>
-         <HeaderThree />
-         <main>
-            <Banner />
-            <Countdown />
-            <Feature />
-            <About />
-            <Event />
-            <TextSlider />
-            <Experience />
-            <Team />
-            <Pricing />
-            <Testimonial />
-            <Brand />
-            <Blog />
-         </main>
-         <FooterOne />
-      </>
-   )
-}
+  return (
+    <>
+      <HeaderThree />
+      <main>
+        <Banner />
+        <Countdown />
+        <Feature />
+        <About />
+        <Schedule />
+        <TextSlider />
+        <Experience />
+        <Team />
+        <Pricing />
+        <Testimonial />
+        <Brand />
+        <Blog />
+      </main>
+      <FooterOne />
+    </>
+  );
+};
 
-export default HomeThree
+export default HomeThree;
--- src/data/ScheduleData.ts
+++ src/data/ScheduleData.ts
@@ -1,117 +1,130 @@
+import bg_img from "@/assets/img/schedule/schedule-4/bg.jpg";
+import { vukafrik_event_data } from "./VukAfrikData";
 
 interface DataType {
-   id: number;
-   schedule_details: {
-      id: number;
-      time: string;
-      time2: string;
-      Topics: string;
-      speaker: string;
-      venue: string;
-   }[]
+  id: number;
+  schedule_details: {
+    id: number;
+    time: string;
+    time2: string;
+    Topics: string;
+    speaker: string;
+    venue: string;
+  }[];
 }
 
-const schedule_data: DataType[] = [
-   {
-      id: 1,
-      schedule_details: [
-         {
-            id: 1,
-            time: "09:00 am",
-            time2: "10:00 am",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-         {
-            id: 2,
-            time: "10:00 am",
-            time2: "11:00 am",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-         {
-            id: 3,
-            time: "11:00 am",
-            time2: "12:00 pm",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-         {
-            id: 4,
-            time: "12:00 pm",
-            time2: "01:00 pm",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-      ],
-   },
-   {
-      id: 2,
-      schedule_details: [
-         {
-            id: 1,
-            time: "9:00 am",
-            time2: "10:00 am",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-         {
-            id: 2,
-            time: "10:00 am",
-            time2: "11:00 am",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-      ],
-   },
-   {
-      id: 3,
-      schedule_details: [
-         {
-            id: 1,
-            time: "9:00 am",
-            time2: "10:00 am",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-         {
-            id: 2,
-            time: "10:00 am",
-            time2: "11:00 am",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-         {
-            id: 3,
-            time: "11:00 am",
-            time2: "12:00 pm",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-      ],
-   },
-   {
-      id: 4,
-      schedule_details: [
-         {
-            id: 1,
-            time: "10:00 am",
-            time2: "11:00 am",
-            Topics: "Women in business conference",
-            speaker: "Ashton Porter",
-            venue: "Manhattan Club nyc",
-         },
-      ],
-   },
+const tab_title: string[] = [
+  "Day 01 - 25 aout, 2026",
+  "Day 02 - 26 aout, 2026",
+  "Day 03 - 27 aout, 2026",
 ];
+
+const schedule_data: DataType[] = [
+  {
+    id: 1,
+    schedule_details: [
+      {
+        id: 1,
+        time: "09:00 am",
+        time2: "10:00 am",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+      {
+        id: 2,
+        time: "10:00 am",
+        time2: "11:00 am",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+      {
+        id: 3,
+        time: "11:00 am",
+        time2: "12:00 pm",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+      {
+        id: 4,
+        time: "12:00 pm",
+        time2: "01:00 pm",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+    ],
+  },
+  {
+    id: 2,
+    schedule_details: [
+      {
+        id: 1,
+        time: "9:00 am",
+        time2: "10:00 am",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+      {
+        id: 2,
+        time: "10:00 am",
+        time2: "11:00 am",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+    ],
+  },
+  {
+    id: 3,
+    schedule_details: [
+      {
+        id: 1,
+        time: "9:00 am",
+        time2: "10:00 am",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+      {
+        id: 2,
+        time: "10:00 am",
+        time2: "11:00 am",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+      {
+        id: 3,
+        time: "11:00 am",
+        time2: "12:00 pm",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+    ],
+  },
+  {
+    id: 4,
+    schedule_details: [
+      {
+        id: 1,
+        time: "10:00 am",
+        time2: "11:00 am",
+        Topics: "Women in business conference",
+        speaker: "Ashton Porter",
+        venue: "Manhattan Club nyc",
+      },
+    ],
+  },
+];
+const title: string = "Notre programme d'événements";
+const sub_title: string = "Programme de l'événement";
+const description: string = `Comme l'année dernière, nous organisons cette année ${vukafrik_event_data.name}. Il s'agit du rendez-vous de tous les leaders`;
+
+export { tab_title, bg_img, title, sub_title, description };
 
 export default schedule_data;
