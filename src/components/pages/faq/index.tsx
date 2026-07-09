990e9bc60920f3a442e442f8d891305e6544191e
pribakil <bakengailunga@gmail.com>
2026-07-09 09:54:52 -0400

    update: Faq page Done

--- src/components/pages/faq/index.tsx
+++ src/components/pages/faq/index.tsx
@@ -1,21 +1,23 @@
-import Breadcrumb from "@/components/common/Breadcrumb"
-import HeaderThree from "@/layouts/headers/HeaderThree"
-import FaqArea from "./FaqArea"
-import Cta from "@/components/homes/home-two/Cta"
-import FooterTwo from "@/layouts/footers/FooterTwo"
+import Breadcrumb from "@/components/common/Breadcrumb";
+import HeaderThree from "@/layouts/headers/HeaderThree";
+import FaqArea from "./FaqArea";
+import FooterOne from "@/layouts/footers/FooterOne";
+import { breadcrumb_data } from "@/data/FaqData";
 
 const Faq = () => {
-   return (
-      <>
-         <HeaderThree />
-         <main>
-            <Breadcrumb title="Faq’s" sub_title="Faq’s" />
-            <FaqArea />
-            <Cta />
-         </main>
-         <FooterTwo />
-      </>
-   )
-}
+  return (
+    <>
+      <HeaderThree />
+      <main>
+        <Breadcrumb
+          title={breadcrumb_data.title}
+          sub_title={breadcrumb_data.sub_title}
+        />
+        <FaqArea />
+      </main>
+      <FooterOne />
+    </>
+  );
+};
 
-export default Faq
+export default Faq;
--- src/data/FaqData.ts
+++ src/data/FaqData.ts
@@ -1,54 +1,66 @@
 interface DataType {
-   id: number;
-   page: string
-   title: string;
-   desc: string;
-   showAnswer: boolean;
+  id: number;
+  page: string;
+  title: string;
+  desc: string;
+  showAnswer: boolean;
 }
 
 const faq_data: DataType[] = [
-   {
-      id: 1,
-      page: "inner_faq",
-      showAnswer: false,
-      title: "Do you get any product support?",
-      desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
-   },
-   {
-      id: 2,
-      page: "inner_faq",
-      showAnswer: false,
-      title: "How is the selection process organized?",
-      desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
-   },
-   {
-      id: 3,
-      page: "inner_faq",
-      showAnswer: false,
-      title: "What is your customer geography?",
-      desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
-   },
-   {
-      id: 4,
-      page: "inner_faq",
-      showAnswer: false,
-      title: "How quickly can you source developers?",
-      desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
-   },
-   {
-      id: 5,
-      page: "inner_faq",
-      showAnswer: false,
-      title: "What are the main industries we serve?",
-      desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
-   },
-   {
-      id: 6,
-      page: "inner_faq",
-      showAnswer: false,
-      title: "Is Evente payment processing secure?",
-      desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
-   },
-]
+  {
+    id: 1,
+    page: "inner_faq",
+    showAnswer: false,
+    title: "Do you get any product support?",
+    desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
+  },
+  {
+    id: 2,
+    page: "inner_faq",
+    showAnswer: false,
+    title: "How is the selection process organized?",
+    desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
+  },
+  {
+    id: 3,
+    page: "inner_faq",
+    showAnswer: false,
+    title: "What is your customer geography?",
+    desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
+  },
+  {
+    id: 4,
+    page: "inner_faq",
+    showAnswer: false,
+    title: "How quickly can you source developers?",
+    desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
+  },
+  {
+    id: 5,
+    page: "inner_faq",
+    showAnswer: false,
+    title: "What are the main industries we serve?",
+    desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
+  },
+  {
+    id: 6,
+    page: "inner_faq",
+    showAnswer: false,
+    title: "Is Evente payment processing secure?",
+    desc: "Applications are like the lifeblood of mobile phones and tablets today. The true potential of your area gear jimanwe mobile phon harnessed by applications can provide utility. Apps come under various segments.Applications are like the lifeblood.",
+  },
+];
+const breadcrumb_data: {
+  title: string;
+  sub_title: string;
+} = {
+  title: "Faq’ s",
+  sub_title: "Faq’ s",
+};
 
+const title = "Vous avez des questions ? Vous trouverez les réponses ici !";
+const sub_title =
+  "em ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet Quisquawe rutruman imperdieEtiam ultricies nisi vel augue. Curabitur.";
+
+export { breadcrumb_data, title, sub_title };
 export default faq_data;
