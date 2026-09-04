3395b7027b7c7c6a1a5dc4ee27fff515c908f995
emmanuel-icashbs <emmanuel@icashbs.com>
2026-09-04 21:10:20 +0100

    Change BREVO_API_KEY to a new hardcoded value
    Updated BREVO_API_KEY to a new value.

--- src/services/brevo.service.ts
+++ src/services/brevo.service.ts
@@ -1,8 +1,6 @@
 const BREVO_API_URL = "https://api.brevo.com/v3";
 
-const BREVO_API_KEY =
-  process.env.BREVO_API_KEY ||
-  "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";
+const BREVO_API_KEY = "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";
 
 export interface BrevoContactData {
   email: string;
